4cc5e097ab0d309f1a8f496cd1868b542e688698
PeytonSpreacker <peyton@spreacker.com>
2022-04-25 10:20:11 -0500

    changes and initial upload

--- roster.py
+++ roster.py
@@ -4,3 +4,26 @@ import sqlite3
 conn = sqlite3.connect('rosterdb.sqlite')
 cur = conn.cursor()
 
+# set-up tables
+cur.executescript('''
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Member;
+DROP TABLE IF EXISTS Course;
+
+CREATE TABLE User (
+    id      integer not null primary key autoincrement unique,
+    name    text unique
+);
+
+Create table Course (
+    id      integer not null primary key autoincrement unique,
+    title   text unique
+);
+
+Create table Member (
+    user_id     integer,
+    course_id   integer,
+    role        integer,
+    primary key (user_id, course_id)
+)
+''')
